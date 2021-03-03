68b6a73b676aa3c511b9e2661add21dda7cd60e4
unknown <steven10695@gmail.com>
2021-03-02 22:44:05 -0500

    header - video - sobrefestival - lineup

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const { series, src, dest, watch, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+
+// Función que compila SASS
+
+const paths ={
+     imagenes: 'src/img/**/*',
+     scss: 'scr/scss/**/*.scss'
+}
+
+function css (  ) {
+     return src(paths.scss)
+     .pipe( sass({
+          outputStyle: 'expanded'
+     }) )
+     .pipe( dest('./build/css') )
+}
+
+function minificarcss(){
+     return src(paths.scss)
+     .pipe( sass({
+          outputStyle: 'compressed'
+     }) )
+     .pipe( dest('./build/css') )
+}
+
+function imagenes(){
+     return src(paths.imagenes)
+          .pipe( imagemin())
+          .pipe(dest('./build/img'))
+          .pipe( notify({ message: 'Imagen Minificada'}));
+}
+
+function versionWebp(){
+     return src(paths.imagenes)
+          .pipe( webp() )
+          .pipe( dest('./build/img'))
+          .pipe( notify({message: 'Version de webP lista'}));
+}
+
+function watchArchivos(){
+     watch( paths.scss, css ); // * = Carpeta Actual -- ** = Todos los archivos con esa extensión
+}
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
+exports.watchArchivos = watchArchivos;
+
+exports.default = series( css, imagenes, versionWebp, watchArchivos);
